3558c970f0a0888fbae7bfb0a55e2c244425c12e
Cgorbenko <acvnrt@gmail.com>
2020-01-27 08:27:33 -0500

    practice

--- exercise_1_14.c
+++ exercise_1_14.c
@@ -1,6 +1,55 @@
 /* Histogram of Frequency characters */ 
-
+/*
 #include <stdio.h>
+#define TNOCHAR 128 //the total number of characters is 128: 0 - 127
+
 int main()
 {
-	int
+	int c, i, j;
+	int character[TNOCHAR];
+
+	for (i=0; i<TNOCHAR; ++i)
+		character[i] = 0;
+
+	while ((c=getchar()) != EOF)
+		++character[c];
+
+	for (i=0; i<TNOCHAR; ++i)
+	{
+		putchar(i);
+
+		for (j=0;j<character[i];++j)
+			putchar('*');
+
+		putchar('\n');
+	}
+	return 0;
+}
+*/
+
+#include <stdio.h>
+#define TNOCHAR 128 // total # of characters
+
+int main()
+{
+	int c, i, j;
+
+	int character[TNOCHAR];
+
+	for (i=0;i<TNOCHAR;++i)
+		character[i] = 0;
+
+	while ((c = getchar()) != EOF)
+		++character[c];
+
+	for (i = 0; i<TNOCHAR; ++i)
+	{
+		putchar(i);
+
+		for (j=0;j<character[i];++j)
+			putchar('*');
+
+		putchar('\n');
+	}
+	return 0;
+}
